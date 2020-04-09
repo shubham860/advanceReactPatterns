511de1aef14e40d085b95fd8ffd157da89e79660
shubham860 <shubham.chauhan860@gmail.com>
2020-04-09 23:00:18 +0530

    update

---
+++ patterns/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Usage from './components/E.propsGetter/Usage';
+
+function App() {
+  return (
+    <div>
+      <Usage/>
+    </div>
+  );
+}
+
+export default App;
